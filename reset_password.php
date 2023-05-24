9816a547c254ab832445ff834bb2f9503a503f7f
Remy <remykohtian@gmail.com>
2023-05-24 21:32:35 +0800

    simple user interface update

--- reset_password.php
+++ reset_password.php
@@ -17,23 +17,68 @@ if (isset($_GET['token'])) {
         if ($expiryTime >= $currentTime) {
             // Token is valid and not expired
             $user_id = $row['user_id'];
+?>
+            <!DOCTYPE html>
+            <html>
 
-            // Display the password reset form
-            echo '
-            <form action="update_password.php" method="POST">
-                <input type="hidden" name="user_id" value="' . $user_id . '">
-                <input type="hidden" name="token" value="' . $token . '">
-                <div>
-                    <label for="new_password">New Password:</label>
-                    <input type="password" id="new_password" name="new_password" required>
-                </div>
-                <div>
-                    <label for="confirm_password">Confirm Password:</label>
-                    <input type="password" id="confirm_password" name="confirm_password" required>
-                </div>
-                <button type="submit">Reset Password</button>
-            </form>
-            ';
+            <head>
+                <title>Password Reset</title>
+                <style>
+                    body {
+                        font-family: Arial, sans-serif;
+                        margin: 0;
+                        padding: 20px;
+                    }
+
+                    h1 {
+                        margin-top: 0;
+                    }
+
+                    form {
+                        max-width: 400px;
+                        margin-top: 20px;
+                    }
+
+                    label,
+                    input {
+                        display: block;
+                        margin-bottom: 10px;
+                    }
+
+                    input[type="password"] {
+                        width: 100%;
+                        padding: 10px;
+                    }
+
+                    button[type="submit"] {
+                        padding: 10px 20px;
+                        background-color: #4CAF50;
+                        color: #fff;
+                        border: none;
+                        cursor: pointer;
+                    }
+                </style>
+            </head>
+
+            <body>
+                <h1>Password Reset</h1>
+                <form action="update_password.php" method="POST">
+                    <input type="hidden" name="user_id" value="<?php echo $user_id; ?>">
+                    <input type="hidden" name="token" value="<?php echo $token; ?>">
+                    <div>
+                        <label for="new_password">New Password:</label>
+                        <input type="password" id="new_password" name="new_password" required>
+                    </div>
+                    <div>
+                        <label for="confirm_password">Confirm Password:</label>
+                        <input type="password" id="confirm_password" name="confirm_password" required>
+                    </div>
+                    <button type="submit">Reset Password</button>
+                </form>
+            </body>
+
+            </html>
+<?php
         } else {
             // Token has expired
             echo 'The password reset link has expired. Please request a new one.';
